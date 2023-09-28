6bc6ab2bcf3903bb09b52ea25aabe9536eedc658
Theo Fortune <theofortunefunction@gmail.com>
2023-09-28 21:51:42 +0100

    Event Binding

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,19 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
+  // name = 'John Smith';
+  addToCart: Number = 0;
   product = {
     name: 'iPhone X',
     price: 789,
     color: 'Red',
     discount: 8.5,
-    inStock: 7
+    inStock: 7,
+    prodImg: 'assets/images/iphone.png',
   };
   getDiscountedPrice() {
-    return this.product.price - (this.product.price * this.product.discount) / 100;
+    return (
+      this.product.price - (this.product.price * this.product.discount) / 100
+    );
   }
   ifInStock() {
     return this.product.inStock > 0
       ? 'Only ' + this.product.inStock + ' items left'
       : 'Out of stock 😔';
   }
+  // onChange(event: any) {
+  //   this.name = event.target.value;
+  // }
 }
