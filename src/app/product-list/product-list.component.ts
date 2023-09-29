287b092e01517aeee5c65b14bfc1c1f49f5f201a
Theo Fortune <theofortunefunction@gmail.com>
2023-09-29 01:43:15 +0100

    ngFor directive

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -6,27 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
-  // name = 'John Smith';
-  addToCart: Number = 0;
-  product = {
-    name: 'iPhone X',
-    price: 789,
-    color: 'Red',
-    discount: 8.5,
-    inStock: 7,
-    prodImg: 'assets/images/iphone.png',
-  };
-  getDiscountedPrice() {
-    return (
-      this.product.price - (this.product.price * this.product.discount) / 100
-    );
-  }
-  ifInStock() {
-    return this.product.inStock > 0
-      ? 'Only ' + this.product.inStock + ' items left'
-      : 'Out of stock 😔';
-  }
-  // onChange(event: any) {
-  //   this.name = event.target.value;
+  // // name = 'John Smith';
+  // addToCart: number = 0;
+  // product = {
+  //   name: 'iPhone X',
+  //   price: 789,
+  //   color: 'Red',
+  //   discount: 8.5,
+  //   inStock: 7,
+  //   prodImg: 'assets/images/iphone.png',
+  // };
+  // getDiscountedPrice() {
+  //   return (
+  //     this.product.price - (this.product.price * this.product.discount) / 100
+  //   );
   // }
+  // ifInStock() {
+  //   return this.product.inStock > 0
+  //     ? 'Only ' + this.product.inStock + ' items left'
+  //     : 'Out of stock 😔';
+  // }
+  // // onChange(event: any) {
+  // //   this.name = event.target.value;
+  // // }
+  // incrementCartValue() {
+  //   if (this.addToCart < this.product.inStock) this.addToCart++;
+  // }
+  // decrementCartValue() {
+  //   if (this.addToCart > 0) this.addToCart--;
+  // }
+  // disableIfZero() {
+  //   if (this.addToCart = 0) {
+
+  //   }
+  // }
+  listOfString: string[] = ['Mark', 'Steve', 'Mary', 'John', 'Sarah'];
 }
